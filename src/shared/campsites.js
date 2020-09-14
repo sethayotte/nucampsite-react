93302f408f00aec9b3f47865752c09b84e1e13f6
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-09-13 20:32:20 -0400

    Updated Comments

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -53,36 +53,36 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5, 
-                text: "What a magnificent view!",
-                author: "Tinus Lorvaldes",
+                text: "What a wonderful trip!",
+                author: "Jack Dorsey",
                 date: "2018-10-25T16:30Z"
             },
             {
                 id: 1,
                 rating: 4, 
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                author: "Brennen Ech",
+                text: "The scenery was beautiful, but it was fairly cloudy the whole time.",
+                author: "Brendan Markson",
                 date: "2017-06-17T03:33Z"
             },
             {
                 id: 2,
                 rating: 5, 
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                author: "McKenzie Sebastian",
+                text: "I caught a huge bass here on my last day.",
+                author: "Derek Shepard",
                 date: "2019-02-18T18:12Z"
             },
             {
                 id: 3,
                 rating: 5, 
-                text: "A wonderful place to reconnect with nature.",
-                author: "Jordan Runn",
+                text: "A great place to disconnect.",
+                author: "Susan Ran",
                 date: "2019-08-04T20:11Z"
             },
             {
                 id: 4,
                 rating: 4, 
-                text: "The stars at night were magnificent!",
-                author: "Ann Dabramov",
+                text: "The night sky was beautiful!",
+                author: "Hannah Pants",
                 date: "2018-07-23T19:44Z"
             }
         ]
