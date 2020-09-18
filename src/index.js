a6a0bfc4ac8c2d618de71befe9e57c27c66af841
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-09-17 20:55:53 -0400

    Adding Header & Footer Components

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(
