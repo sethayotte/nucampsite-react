120825b5cfbecb05876d9ac1b387ad9df62064cf
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-09-18 20:25:18 -0400

    React Router & SPA Navigation

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({ campsite, onClick }) {
+function RenderDirectoryItem({ campsite }) {
   return (
-    <Card onClick={() => onClick(campsite.id)}>
-      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-      <CardImgOverlay>
-        <CardTitle>{campsite.name}</CardTitle>
-      </CardImgOverlay>
+    <Card>
+      <Link to={`/directory/${campsite.id}`}>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+          <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
@@ -15,14 +18,26 @@ function Directory(props) {
     const directory = props.campsites.map((campsite) => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+          <RenderDirectoryItem campsite={campsite} />
         </div>
       );
     });
 
     return (
       <div className="container">
-        <div className="row">{directory}</div>
+        <div className="row">
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+              <BreadcrumbItem active>Directory</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>Directory</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          {directory}
+        </div>
       </div>
     );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from "./CampsiteInfoComponent";
 import Footer from './FooterComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({ selectedCampsite: campsiteId });
-      }
-
     render() {
+        const HomePage = () => {
+            return (
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+            )
+        }
+
         return (
             <div>
                 <Header />
-                <Directory campsites = {this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites = {this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
