e5462877c968ab1aae19b55b77d02c3f00f92926
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-09-19 12:10:35 -0400

    About Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -48,6 +49,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites = {this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners = {this.state.partners} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
