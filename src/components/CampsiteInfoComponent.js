234586819625beb704e766b9da96a0e8580c4f7d
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-10-02 14:01:28 -0400

    json-server - updating redux & components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -15,6 +15,7 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import {Loading} from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -24,8 +25,7 @@ function RenderCampsite({ campsite }) {
   return (
     <div class="col-md-5 m-1">
       <Card>
-        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardBody>
+      <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />        <CardBody>
           <CardText>{campsite.description}</CardText>
         </CardBody>
       </Card>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,45 +1,48 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import {Loading} from './LoadingComponent';
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
-function RenderCard({item, isLoading, errMess}) {
-    if (isLoading) {
-        return <Loading />;
-    }
-    if (errMess) {
-        return <h4>{errMess}</h4>
-    }
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    )
+function RenderCard({ item, isLoading, errMess }) {
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (errMess) {
+    return <h4>{errMess}</h4>;
+  }
+  return (
+      
+    <Card>
+        {console.log(baseUrl + item.image)}
+      <CardImg src={baseUrl + item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function Home(props) {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md m-1">
-                    <RenderCard 
-                    item={props.campsite} 
-                    isLoading={props.campsitesLoading}
-                    errMess={props.campsitesErrMess}
-                    />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md m-1">
+          <RenderCard
+            item={props.promotion}
+            isLoading={props.promotionLoading}
+            errMess={props.promotionErrMess}
+          />
         </div>
-    );
+        <div className="col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+        <div className="col-md m-1">
+          <RenderCard item={props.partner} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
